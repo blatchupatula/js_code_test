f46cccfd81e186b664058e53069b07fbbdfab8d2
bhanu <bhanulatchupatula@gmail.com>
2020-12-01 22:42:28 +0530

    pagination issues resolved

--- js_code_test/script.js
+++ js_code_test/script.js
@@ -6,9 +6,11 @@ jQuery(document).ready(function($){
         }
     });
 
-    $(".add-user-btn").click(function(e){
+    $(document).on('click','.add-user-btn',function(){
+    // $(".add-user-btn").click(function(e){
         var user_name = $(".user-name").val();
         var user_email = $(".user-email").val();
+        var page_num = $(this).attr('page');
 
         $.ajax({
             url: "https://jsonplaceholder.typicode.com/users/",
@@ -23,7 +25,8 @@ jQuery(document).ready(function($){
             success: function (data) {
                 $('#addUser').modal('hide');
                 $('#myTable tr:last').after('<tr name-'+ data['id'] +'="'+ user_name +'" email-'+ data['id'] +'="'+ user_email +'" class="test option-'+ data['id'] +'"><td class="td-'+data['id']+'"><input type="checkbox" value="'+ data['id'] +'" class="check_input" name="check"></td><td class=user-id-'+data['id']+'>'+ data['id'] +'</td><td class=user-name-'+data['id']+'>'+ user_name +'</td><td class=user-email-'+data['id']+'>'+ user_email +'</td></tr>');
-                showTr();
+                $(".pagination-"+page_num).click();
+                showTr(page_num);
             }
         });
     })
@@ -34,6 +37,7 @@ jQuery(document).ready(function($){
     });
 
     $('.user-delete-btn').click(function(event){
+        var page_num = $(this).attr('page');
         event.preventDefault();
         var searchIDs = [];
         $("#myTable input:checkbox:checked").map(function(){
@@ -51,7 +55,11 @@ jQuery(document).ready(function($){
                 },
                 success: function (data) {
                     $(".option-"+searchIDs[i]).remove();
-                    showTr();
+                    if($('#myTable tr.test').length % 4 == 0){
+                        page_num = page_num - 1;
+                    }
+                    $(".pagination-"+page_num).click();
+                    showTr(page_num);
                 }
             })
         })
@@ -115,28 +123,36 @@ jQuery(document).ready(function($){
         
     })
 
+    $(".add-user-modal").click(function(){
+        var page_num = $(".active").attr('rel');
+        $(".add-user-btn").attr('page', page_num);
+    });
 
     setTimeout(showTr, 1000)
 
-    function showTr() {
+    function showTr(page_num=0) {
         $("body").show();
         $('#nav').remove();
         $('#myTable').after('<div class="text-right" id="nav"></div>');
-        var rowsShown = 5;
+        var rowsShown = 4;
         var rowsTotal = $('#myTable tr.test').length;
         var numPages = rowsTotal/rowsShown;
         for(i = 0;i < numPages;i++) {
             var pageNum = i + 1;
-            $('#nav').append('<a href="#" rel="'+i+'">'+pageNum+'</a> ');
+            $('#nav').append('<a class="pagination-'+ i +'" href="#" rel="'+i+'">'+pageNum+'</a> ');
         }
-        $('#myTable tr.test').slice(0, rowsShown).show();
-        $('#nav a:first').addClass('active');
+        if(page_num == 0){
+            $('#myTable tr.test').slice(0, rowsShown).show();
+        }      
+        $('.pagination-'+page_num).addClass('active');
+        $(".user-delete-btn").attr('page', page_num);   
         $('#nav a').bind('click', function(){
             $('#nav a').removeClass('active');
             $(this).addClass('active');
             var currPage = $(this).attr('rel');
             var startItem = currPage * rowsShown;
             var endItem = startItem + rowsShown;
+            $(".user-delete-btn").attr('page', currPage);
             $('#myTable tr.test').css('opacity','0.0').hide().slice(startItem, endItem).
             css('display','table-row').animate({opacity:1}, 300);
         });
